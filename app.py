82a27e715266b0e4319bc1f0fda7127ce870c41b
Akram <akrommuhammadyusuf@gmail.com>
2026-02-19 12:25:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 
+import tkinter as tk
+from datetime import datetime
+
+from config import *
+from utils.clock      import start_clock
+from utils.typewriter import start_typewriter
+from tabs import BattlesTab, FiguresTab, TimelineTab, QuotesTab, IntelTab
