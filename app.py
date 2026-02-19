c07976bfc3064d743212b26bbd95dfcde41f2a48
Akram <akrommuhammadyusuf@gmail.com>
2026-02-19 12:26:14 +0700

    Implement WW2App class for application assembly
    Added WW2App class to manage application components.

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ from config import *
 from utils.clock      import start_clock
 from utils.typewriter import start_typewriter
 from tabs import BattlesTab, FiguresTab, TimelineTab, QuotesTab, IntelTab
+
+class WW2App:
+    """
+    Assembler utama aplikasi. Bertanggung jawab atas:
+      - header & navigation bar
+      - menginstansiasi setiap tab
+      - status bar bawah
+      - clock & typewriter effect
